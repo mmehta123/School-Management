c01efe8235da5b01f27a157d9d2e2927cbc11b9a
mmehta123 <mohitmehta285@gmail.com>
2023-11-09 00:00:43 +0530

    student api created  re-admission slc edit profile,view Profile

--- Backend/controllers/studentController.js
+++ Backend/controllers/studentController.js
@@ -1,6 +1,6 @@
 const { Student, GlobalStudent } = require("../models/student");
 const errorHandler = require("../utils/error");
-
+/////////////////////////Student Admission////////////////////
 const add = async (req, res, next) => {
   const { name, fathername, dob, aadhar, mothername, rollno, standard } =
     req.body;
@@ -11,7 +11,6 @@ const add = async (req, res, next) => {
   // do {
   req.body.srn = Math.floor(Math.random() * (10000000 - 1000 + 1)) + 1000;
   // } while ((await GlobalStudent.find({ srn: req.body.srn })) !== null);
-  console.log(req.body);
 
   //if user exists with same aadhar card in same school
   try {
@@ -59,8 +58,164 @@ const add = async (req, res, next) => {
     return next(e);
   }
 };
-const view = (req, res, next) => {};
-const issueSlc = (req, res, next) => {};
-const editStudentProfile = (req, res, next) => {};
+/////////////////////////Student Profile View////////////////////
+const view = async (req, res, next) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (!student) {
+      return next(errorHandler(404, "student not found"));
+    }
+    if (req.user.id !== student.parentSchoolId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    return res.status(200).json({ success: true, student });
+  } catch (error) {
+    next(error);
+  }
+};
+/////////////////////////Student ReAdmission////////////////////
+const reAdmission = async (req, res, next) => {
+  try {
+    if (req.body.hasOwnProperty("srn") || req.body.hasOwnProperty("aadhar")) {
+      // here we found student with addhar or srn and checked its active must be false(not in any school means have slc)
+      const student = await GlobalStudent.findOne({
+        $or: [{ srn: req.body?.srn }, { aadhar: req.body?.aadhar }],
+        active: false,
+      });
+      if (!student)
+        return next(
+          errorHandler(404, "student not found / Or does not have SLC")
+        );
+      // Generate a new unique roll number based on the student's standard
+      //this function give us local latest student of same standard as we want to admit new student in.means give us last roll no of class 
+      const latestStudent = await Student.findOne({
+        standard: student.standard,
+      })
+        .sort({ rollno: -1 })
+        .limit(1);
 
-module.exports = { add, view, issueSlc, editStudentProfile };
+      let newRollNumber = 1; // Default if no previous student in the same standard
+      if (latestStudent) {
+        newRollNumber = Number(latestStudent.rollno) + 1;
+      }
+
+      const newStudent = {
+        name: student.name,
+        dob: student.dob,
+        mothername: student.mothername,
+        fathername: student.fathername,
+        srn: Number(student.srn),
+        aadhar: Number(student.aadhar),
+        active: true,
+        parentSchoolId: req.user.id,
+        standard: Number(student.standard),
+        rollno: Number(newRollNumber),
+      };
+      // admitted it in our school means added in local DB and updated student parentScholId to current admitting school in Glocbal DB
+      const admittedStudent = await new Student(newStudent).save();
+      await GlobalStudent.findByIdAndUpdate(
+        student.id,
+        {
+          $set: {
+            active: true,
+            parentSchoolId: req.user.id,
+            rollno: newRollNumber,
+          },
+        },
+        { new: true }
+      );
+      return res.status(200).json({
+        success: true,
+        student: admittedStudent,
+        message: "student Readmission Succesful",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "please provide srn or aadhar",
+      });
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/////////////////////////Student SLC ////////////////////
+const issueSlc = async (req, res, next) => {
+  try {
+    const student = await findOne({ srn: req.params.srn });
+    if (!student) {
+      return res
+        .status(404)
+        .json({ success: false, message: "student not found" });
+    }
+    if (req.user.id !== student.parentSchoolId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    // deltet student from local (School DB)
+    await Student.findOneAndDelete({ srn: req.params.srn });
+    // updated details of current student to active false(means not in school now and student current school to xxxxxxx)
+    await GlobalStudent.findOneAndUpdate(
+      { srn: req.params.srn },
+      { $set: { active: false, parentSchoolId: "xxxxxxxxxxxxx" } },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+      student: {
+        message:
+          "student is availible for reAdmission Please note Student's Registration No.",
+        srn: student.srn,
+      },
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+/////////////////////////Student Profile Updatation////////////////////
+const editStudentProfile = async (req, res, next) => {
+  try {
+    req.params.srn = Number(req.params.srn);
+    if (
+      req.body.hasOwnProperty("aadhar") ||
+      req.body.hasOwnProperty("srn") ||
+      req.body.hasOwnProperty("rollno") ||
+      req.body.hasOwnProperty("standard")
+    ) {
+      return next(
+        errorHandler(403, "you can not change srn,aadhar,rollno,standard")
+      );
+    }
+    const studentAuth = await Student.findOne({ srn: req.params.srn });
+    if (!student) {
+      return res
+        .status(404)
+        .json({ success: false, message: "student not found" });
+    }
+    if (req.user.id !== studentAuth.parentSchoolId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    //update student in our local DB
+    const student = await Student.findOneAndUpdate(
+      { srn: req.params.srn },
+      { $set: req.body },
+      {
+        new: true,
+      }
+    );
+    //did same for Global Db
+    await GlobalStudent.findOneAndUpdate(
+      { srn: req.params.srn },
+      { $set: req.body },
+      {
+        new: true,
+      }
+    );
+    return res.status(200).json({ success: true, student });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { add, view, issueSlc, editStudentProfile, reAdmission };
--- Backend/routes/studentRoutes.js
+++ Backend/routes/studentRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { add,view,issueSlc,editStudentProfile } = require("../controllers/studentController.js");
+const { add,view,issueSlc,editStudentProfile,reAdmission } = require("../controllers/studentController.js");
 const verifyToken = require("../utils/verifyToken.js");
 
 
 router.get("/:id",verifyToken,view);
 router.post("/add",verifyToken,add);
-router.post("/issueSlc/:id",verifyToken,issueSlc);
-router.post("/editStudentProfile/:id",verifyToken,editStudentProfile);
+router.post("/issueSlc/:srn",verifyToken,issueSlc);
+router.post("/readmission",verifyToken,reAdmission);
+router.post("/editStudentProfile/:srn",verifyToken,editStudentProfile);
 
 
 module.exports = router;
